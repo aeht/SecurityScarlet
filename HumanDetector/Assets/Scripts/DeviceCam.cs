e439da77abf0a28c4ef5388d7bfb31a6c8a532bf
aeht <tvtavila@gmail.com>
2020-12-20 23:48:03 -0500

    First push w/ pc cam and date and time stamp

---
+++ HumanDetector/Assets/Scripts/DeviceCam.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeviceCam : MonoBehaviour
+{
+    static WebCamTexture deviceCamera; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(deviceCamera == null)
+        {
+            deviceCamera = new WebCamTexture();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<RawImage>().texture = deviceCamera; 
+        deviceCamera.Play();
+    }
+}
+
---
+++ HumanDetector/Assets/Scripts/UpdateTime.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateTime : MonoBehaviour
+{
+    public Text timeDisplay; 
+    public Text dateDisplay; 
+    private int hour; 
+    private int minutes;
+    private int month;
+    private int day;
+    private int year; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        month = System.DateTime.Now.Month;
+        day   = System.DateTime.Now.Day; 
+        year  = System.DateTime.Now.Year; 
+        dateDisplay.GetComponent<Text>().text = "Date: " + month + "/" + day + "/" + year; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hour = (System.DateTime.Now.Hour)-12; 
+        minutes = System.DateTime.Now.Minute;
+        timeDisplay.GetComponent<Text>().text = "Time: " + hour + ":" + minutes;
+    }
+}
